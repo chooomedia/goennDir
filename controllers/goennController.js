a48694e7167fa6fb3765aaf199cb3b0cc69a2981
Chris Matt <chooom@gmx.de>
2018-10-05 23:08:23 +0200

    first final stage

--- controllers/goennController.js
+++ controllers/goennController.js
@@ -4,6 +4,7 @@ app.controller('goennController', [
     '$http',
     '$timeout',
     '$window',
+    '$mdSidenav',
     '$location',
     'goennService', function (
         $scope,
@@ -11,8 +12,59 @@ app.controller('goennController', [
         $http,
         $timeout,
         $window,
+        $mdSidenav,
         $location,
         goennService) {
+
+            $scope.toggleLeft = buildDelayedToggler('left');
+            $scope.toggleRight = buildToggler('right');
+            $scope.isOpenRight = function(){
+              return $mdSidenav('right').isOpen();
+            };
+        
+            /**
+             * Supplies a function that will continue to operate until the
+             * time is up.
+             */
+            function debounce(func, wait, context) {
+              var timer;
+        
+              return function debounced() {
+                var context = $scope,
+                    args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                  timer = undefined;
+                  func.apply(context, args);
+                }, wait || 10);
+              };
+            }
+        
+            /**
+             * Build handler to open/close a SideNav; when animation finishes
+             * report completion in console
+             */
+            function buildDelayedToggler(navID) {
+              return debounce(function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                  .toggle()
+                  .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                  });
+              }, 200);
+            }
+        
+            function buildToggler(navID) {
+              return function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                  .toggle()
+                  .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                  });
+              };
+            }
 
         $scope.searchField = "Suche z.B. nach Festival";
 
@@ -154,10 +206,8 @@ app.controller('goennController', [
             {title: 'Welcome to Paw AngularJS App'},
             {content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'}
         ];
-
     }
 ])
-
     .factory('goennService', function () {
         return function (data) {
             if (data.length >= 3) {
@@ -170,4 +220,23 @@ app.controller('goennController', [
 
     .config(['$qProvider', function ($qProvider) {
         $qProvider.errorOnUnhandledRejections(false);
-    }]);
+    }])
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav('left').close()
+            .then(function () {
+              $log.debug("close LEFT is done");
+            });
+    
+        };
+      })
+      .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav('right').close()
+            .then(function () {
+              $log.debug("close RIGHT is done");
+            });
+        };
+      });
